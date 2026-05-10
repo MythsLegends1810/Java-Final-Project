4602992d169bf16ffdf47b24caab8f15bc6c288d
gorham_0976129 <0976129@MY.SCCCD.EDU>
2026-05-09 18:13:39 -0700

    Feature was working on vortex bullet pattern logic

--- Projectile.java
+++ Projectile.java
@@ -1,29 +1,36 @@
 //Extension of Entity.java will contain the math and patterns for the attacks (example sin waves and stuff like that similar to Terrarias calamity fights
 //Think like Empress of light attacks etc
-public class Projectile {
-	public double x, y, vx, vy; //(x, y) position on the screen and (vx, vy) will be the velocity of the Projectile
-	public boolean active; //This will be like the on/off switch for the GC
+public class Projectile extends Entity {
+	public double angle;
+	public double speed;
+	public double rotationSpeed; //This will be how much the bullets turn every frame
 
-	public Projectile() { //This will be so when we spawn the bullets we set them to false
-		this.active = false;
-	}
-
-	public void spawn(double x, double y, double vx, double vy) {
-		x = x;
-		y = y;
-		vx = vx;
-		vy = vy;
-		active = true;
+	public void spawn(double x, double y, double angle, duble speed, double rotationSpeed) {
+		this.x = x;
+		this.y = y;
+		this.agnle = angle;
+		this.speed = speed;
+		this.rotationSpeed = rotationSpeed; // This needs to be set to 1 or 2 for a curve but test others depedning on the difficulty
+		this.//add the entity alive variable here.
 	}
 
 	public void update() {
-		if (!active) return;
+		if (!//add the entity alive variable here) return;
+		
+		//change the angle slightly  every frame
+		angle += rotationSpeed;
+		
+		//this will be the velocity based on the new angles
+		double rad = //need to add the math for radians here
+		vx = 
+		vy =
 
+		//Move
 		x += vx;
 		y += vy;
 
-		if (x < -50 || x > 850 || y < -50 || y > 650) { //This is the bounds check temporary values
-			active = false;
-		}
+		//out of bounds check
+		if (x < -50 || x > 850 || y < -50 || y > 650) {
+			//entity alive variable here = false
 	}
 }
